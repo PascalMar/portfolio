ff73921b6cc1c53d7e8f6190447be1ceff627b16
PascalMar <pa.marienfeld@gmail.com>
2023-08-10 17:43:15 +0200

    added header and home component

---
+++ src/app/header/header.component.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-header',
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.scss']
+})
+export class HeaderComponent {
+
+
+  animateBurger() {
+    const button = document.querySelector('.hamburger');
+    if (button) {
+      button.classList.toggle('is-active');
+    }
+  }
+}
