1914c58a472d1cda63f1dee6eebb2e1257da19d9
PascalMar <pa.marienfeld@gmail.com>
2023-08-24 16:50:52 +0200

    tried to add routerlink

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,6 +3,7 @@ import { RouterModule, Routes } from '@angular/router';
 import { MySkilsComponent } from './my-skils/my-skils.component';
 import { AboutMeComponent } from './about-me/about-me.component';
 
+
 const routes: Routes = [  
     { path: 'about-me', component: AboutMeComponent },
     { path: 'my-skills', component: MySkilsComponent },
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import {RouterModule, Routes} from '@angular/router'
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -7,6 +8,10 @@ import { HomeComponent } from './home/home.component';
 import { HeaderComponent } from './header/header.component';
 import { AboutMeComponent } from './about-me/about-me.component';
 import { MySkilsComponent } from './my-skils/my-skils.component';
+
+const routes: Routes = [
+  {path: 'my-skills', component: MySkilsComponent}
+]
 
 @NgModule({
   declarations: [
@@ -18,7 +23,13 @@ import { MySkilsComponent } from './my-skils/my-skils.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot(
+      routes, {
+        enableTracing: false,
+        anchorScrolling: 'enabled'
+      }
+    )    
   ],
   providers: [],
   bootstrap: [AppComponent]
