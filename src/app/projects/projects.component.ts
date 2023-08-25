2daee9b9101f61e34e4c4db156facdccb4bd5536
PascalMar <pa.marienfeld@gmail.com>
2023-08-25 16:06:39 +0200

    added informations for the projects

--- src/app/projects/projects.component.ts
+++ src/app/projects/projects.component.ts
@@ -10,15 +10,21 @@ export class ProjectsComponent {
   projects = [
     {
       picture: '/assets/img/about-me/projects/join.png',
-      headline: 'Join'
+      headline: 'Join',
+      technologies: 'JavaScript | HTML | CSS',
+      description: 'Task manager inspired by the Kanban System. Create and organize tasks using drag and drop functions, assign users and categories.'
     },
     {
       picture: '/assets/img/about-me/projects/el-pollo-loco.png',
-      headline: 'El-Pollo-Loco'
+      headline: 'El-Pollo-Loco',
+      technologies: 'JavaScript | HTML | CSS',
+      description: 'A simple Jump-and-Run game based on an object-oriented approach. Help Pepe to fight against the Boss Chicken and his crew.'
     },
     {
       picture: '/assets/img/about-me/projects/crm.png',
-      headline: 'Simple CRM'
+      headline: 'Simple CRM',
+      technologies: 'Angular | Firebase | Material Design | Api',
+      description: 'A very Simple Customer Relationship Management system working with CRUD functionality. '
     }
   ]
 
