811f90f6112f64c173a3e5351a264ecd72d2b069
PascalMar <pa.marienfeld@gmail.com>
2023-09-05 14:48:11 +0200

    worked on responsive for max-width 1150px

--- src/app/projects/projects.component.ts
+++ src/app/projects/projects.component.ts
@@ -12,19 +12,33 @@ export class ProjectsComponent {
       picture: '/assets/img/about-me/projects/join.png',
       headline: 'Join',
       technologies: 'JavaScript | HTML | CSS',
-      description: 'Task manager inspired by the Kanban System. Create and organize tasks using drag and drop functions, assign users and categories.'
+      description: 'Task manager inspired by the Kanban System. Create and organize tasks using drag and drop functions, assign users and categories.',
+      github: '',
+      livetest: ''
     },
     {
       picture: '/assets/img/about-me/projects/el-pollo-loco.png',
       headline: 'El-Pollo-Loco',
       technologies: 'JavaScript | HTML | CSS',
-      description: 'A simple Jump-and-Run game based on an object-oriented approach. Help Pepe to fight against the Boss Chicken and his crew.'
+      description: 'A simple Jump-and-Run game based on an object-oriented approach. Help Pepe to fight against the Boss Chicken and his crew.',
+      github: '',
+      livetest: ''
     },
     {
       picture: '/assets/img/about-me/projects/crm.png',
       headline: 'Simple CRM',
       technologies: 'Angular | Firebase | Material Design | Api',
-      description: 'A very Simple Customer Relationship Management system working with CRUD functionality. '
+      description: 'A very Simple Customer Relationship Management system working with CRUD functionality. ',
+      github: '',
+      livetest: ''
+    },
+    {
+      picture: '/assets/img/about-me/projects/portfolio.png',
+      headline: 'My personal Portfolio',
+      technologies: 'Angular | HTML | SCSS',
+      description: 'My personal Website created with Angular and SCSS. ',
+      github: 'https://github.com/PascalMar/portfolio',
+      livetest: '#top'
     }
   ]
 
