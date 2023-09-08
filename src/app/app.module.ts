04de2de28081ca12c474549d90e744a51b5381f6
PascalMar <pa.marienfeld@gmail.com>
2023-09-08 11:36:50 +0200

    added animation for header

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,7 @@ import { FormsModule } from '@angular/forms';
 import {ReactiveFormsModule} from '@angular/forms';
 import { FooterComponent } from './footer/footer.component';
 import { LegalNoticeComponent } from './legal-notice/legal-notice.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 
 
@@ -33,7 +34,8 @@ import { LegalNoticeComponent } from './legal-notice/legal-notice.component';
     BrowserModule,
     AppRoutingModule,
     FormsModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,9 +1,31 @@
-import { Component} from '@angular/core';
+import { Component } from '@angular/core';
+import { trigger, state, style, animate, transition } from '@angular/animations';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.scss']
+  styleUrls: ['./header.component.scss'],
+  animations: [
+    trigger('slideInOut', [
+      state('in', style({
+        transform: 'translateY(0)',
+        opacity: 1
+      })),
+      transition('void => *', [
+        style({
+          transform: 'translateY(100%)',
+          opacity: 0
+        }),
+        animate('300ms ease-out')
+      ]),
+      transition('* => void', [
+        animate('300ms ease-in', style({
+          transform: 'translateY(100%)',
+          opacity: 0
+        }))
+      ])
+    ])
+  ]
 })
 export class HeaderComponent {
 
