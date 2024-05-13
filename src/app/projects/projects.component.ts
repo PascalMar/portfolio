448551dc09cd09a6d4267d09048a985f1c85e289
Pascal Marienfeld <pa.marienfeld@gmail.com>
2024-05-13 17:34:03 +0200

    added new project

--- src/app/projects/projects.component.ts
+++ src/app/projects/projects.component.ts
@@ -17,14 +17,6 @@ export class ProjectsComponent {
       livetest: 'https://join.pascal-marienfeld.de/'
     },
     {
-      picture: '/assets/img/about-me/projects/el-pollo-loco1.png',
-      headline: 'El-Pollo-Loco',
-      technologies: 'JavaScript | HTML | CSS',
-      description: 'A simple Jump-and-Run game based on an object-oriented approach. Help Pepe to fight against the Boss Chicken and his crew.',
-      github: 'https://github.com/PascalMar/El-Pollo-Loco',
-      livetest: 'https://el-pollo-loco.pascal-marienfeld.de/'
-    },
-    {
       picture: '/assets/img/about-me/projects/crm.png',
       headline: 'Simple CRM',
       technologies: 'Angular | Firebase | Material Design | Api | Bootstrap',
@@ -39,6 +31,14 @@ export class ProjectsComponent {
       description: 'A basic full-stack app allowing users to log in, view a list of movies, access more details about each movie, mark favorites, edit their profile, and see their favorited films.',
       github: 'https://github.com/PascalMar/myFlix-Client',
       livetest: 'https://pa-mar-myflix.netlify.app/'
+    },
+    {
+      picture: '/assets/img/about-me/projects/meet.png',
+      headline: 'Meet',
+      technologies: 'React | Rechart | OAuth2.0 | AWS Lambda | Serverless functions',
+      description: 'This PWA, built via TTD, enables user to login via OAuth2.0 using their Google account to access a list of events. The list can be filtered by location (city) and the number of events to display. It utilizes serverless functions (AWS lambda) for obtaining the access token from the authorization server and for fetching the list of events from the Google calendar API.',
+      github: 'https://github.com/PascalMar/meet',
+      livetest: 'https://pascalmar.github.io/meet/'
     }
   ]
 
